368698c8b69b94db94ad0f92977cda906a849886
Sebastian Kussl <sebastian.kussl@yahoo.de>
2019-05-28 16:22:25 +0200

    feat: add deployment folder

---
+++ docs/precache-manifest.f8eef17ed30c4668e51a23a794d53993.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3ffdcf6b3e0e3bb80e6275ac470b411",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc2def178e8c7a60b182",
+    "url": "/static/css/main.7bb91b9a.chunk.css"
+  },
+  {
+    "revision": "5052da522cba703a5bb1",
+    "url": "/static/js/2.b68966f9.chunk.js"
+  },
+  {
+    "revision": "fc2def178e8c7a60b182",
+    "url": "/static/js/main.6a8f4eb3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
